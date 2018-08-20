6fa17f7b440f5b3c01e6e9afdbbaecd816cc44e2
blakemecham <bam.mecham@gmail.com>
2018-08-20 12:07:30 -0600

    header added

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className= 'header'>
+            <h1>Bottega MadLibs</h1>
+            <p>This is cool.</p>
+        </div>
+    )
+}
+
+export default Header;
