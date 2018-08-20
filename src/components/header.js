877c51f2fcd08fa7c012e0f0a118b4271ae5219d
blakemecham <bam.mecham@gmail.com>
2018-08-20 15:57:07 -0600

    two way binding with inputs

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className= 'header'>
-            <h1>Bottega MadLibs</h1>
+            <h1>MadLib!</h1>
             <p>Fill it out!</p>
         </div>
     )
