55d53c867d2f0a660d386c4ba5321aa73f1a2a7f
blakemecham <bam.mecham@gmail.com>
2018-08-20 14:01:53 -0600

    first component for card

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className= 'header'>
             <h1>Bottega MadLibs</h1>
-            <p>This is cool.</p>
+            <p>Fill it out!</p>
         </div>
     )
 }
