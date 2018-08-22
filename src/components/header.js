c59a4de291314e71bcf1378916efce13c402ebf4
blakemecham <bam.mecham@gmail.com>
2018-08-21 19:42:33 -0600

    skew added to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className= 'header'>
-            <h1>MadLib!</h1>
-            <p>Fill it out!</p>
+        <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
     )
 }
